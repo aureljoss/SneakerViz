a61f706e6587c5eab0d883b7c8bb1d7274478dbd
Aurelie <josserand.n.aurelie@gmail.com>
2026-01-04 10:11:49 -0500

    Updates to navigation and added colors for main body

--- src/components/ShoeBuilder.jsx
+++ src/components/ShoeBuilder.jsx
@@ -1,7 +1,7 @@
 export default function ShoeBuilder(props) {
   return (
     <>
-      <h2>{props.title}</h2>
+      <h1>{props.title}</h1>
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -14,6 +14,8 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Fab from "@mui/material/Fab";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
@@ -44,12 +46,12 @@ function App() {
           <Box
             sx={{
               bgcolor: "rgba(255, 255, 255, 0.4)",
-              borderRadius: "20px",
-              padding: "30px",
+              borderRadius: "15px",
+              padding: "10px",
               backdropFilter: "blur(10px)",
               display: "flex",
               flexDirection: "column",
-              alignItems: "center",
+              alignItems: "left",
               maxHeight: "80vh",
               marginBottom: "10px",
             }}
@@ -61,63 +63,112 @@ function App() {
               you shine. A subtle platform gives you just the right amount of
               height.
             </p>
+            <p>
+              <span className="stars">★ ★ ★ ★ ☆</span> 4.6
+            </p>
           </Box>
           <Box
             sx={{
               bgcolor: "rgba(255, 255, 255, 0.4)",
-              borderRadius: "20px",
+              borderRadius: "15px",
               padding: "10px",
               backdropFilter: "blur(10px)",
               display: "flex",
               flexDirection: "column",
-              alignItems: "center",
+              alignItems: "left",
+              maxHeight: "80vh",
+              marginBottom: "10px",
+            }}
+          >
+            <h1>Product Details</h1>
+            <p>Fits large; we recommend ordering a half size down</p>
+            <p>
+              Free standard shipping on orders $50+ and free 60-day returns for
+              Nike Members. Learn more. Return policy exclusions apply.
+            </p>
+          </Box>
+          <Box
+            sx={{
+              bgcolor: "rgba(255, 255, 255, 0.4)",
+              borderRadius: "15px",
+              padding: "10px",
+              backdropFilter: "blur(10px)",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "left",
               maxHeight: "80vh",
             }}
           >
-            <ShoeBuilder title="Vamp / Quarter 1/10" />
+            <div class="builder-navigation">
+              <ArrowBackIosIcon onClick={() => {}} />
+              <ShoeBuilder title="Vamp / Quarter" />
+              <ArrowForwardIosIcon />
+            </div>
+
             <div>
-              <Fab
-                size="small"
-                sx={{
-                  backgroundColor: "#8B4513",
-                  margin: "10px",
-                  border: mainColor === "#8B4513" ? "2px solid #727272ff" : null,
-                }}
-                onClick={() => setColor("#8B4513")}
-              ></Fab>
-              <Fab
-                size="small"
-                sx={{
-                  backgroundColor: "#ffffff",
-                  margin: "10px",
-                  border: mainColor === "#ffffff" ? "2px solid #727272ff" : null,
-                }}
-                onClick={() => setColor("#ffffff")}
-              ></Fab>
               <Fab
                 size="small"
                 sx={{
                   backgroundColor: "#000000",
                   margin: "10px",
-                  border: mainColor === "#000000" ? "2px solid #727272ff" : null,
+                  transform: "scale(0.9)",
+                  border:
+                    mainColor === "#000000" ? "2px solid #353535ff" : null,
                 }}
                 onClick={() => setColor("#000000")}
               ></Fab>
               <Fab
                 size="small"
                 sx={{
+                  backgroundColor: "#ffffff",
+                  margin: "10px",
+                  transform: "scale(0.9)",
+                  border:
+                    mainColor === "#ffffff" ? "2px solid #353535ff" : null,
+                }}
+                onClick={() => setColor("#ffffff")}
+              ></Fab>
+              <Fab
+                size="small"
+                sx={{
+                  backgroundColor: "#d19c2a",
+                  margin: "10px",
+                  transform: "scale(0.9)",
+                  border:
+                    mainColor === "#d19c2a" ? "2px solid #353535ff" : null,
+                }}
+                onClick={() => setColor("#daa431")}
+              ></Fab>
+              <Fab
+                size="small"
+                sx={{
                   backgroundColor: "#cb8bdf",
                   margin: "10px",
-                  border: mainColor === "#cb8bdf" ? "2px solid #727272ff" : null,
+                  transform: "scale(0.9)",
+                  border:
+                    mainColor === "#cb8bdf" ? "2px solid #353535ff" : null,
                 }}
                 onClick={() => setColor("#cb8bdf")}
               ></Fab>
               <Fab
                 size="small"
                 sx={{
+                  backgroundColor: "#647be0",
+                  margin: "10px",
+                  transform: "scale(0.9)",
+                  border:
+                    mainColor === "#647be0" ? "2px solid #353535ff" : null,
+                }}
+                onClick={() => setColor("#647be0")}
+              ></Fab>
+              <Fab
+                size="small"
+                sx={{
                   backgroundColor: "#cc1d51",
                   margin: "10px",
-                  border: mainColor === "#cc1d51ff" ? "2px solid #727272ff" : null,
+                  transform: "scale(0.9)",
+                  border:
+                    mainColor === "#cc1d51ff" ? "2px solid #353535ff" : null,
                 }}
                 onClick={() => setColor("#cc1d51")}
               ></Fab>
