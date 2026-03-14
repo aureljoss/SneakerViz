28840cb0eec8347a21ba67610cf8ea853cd151d4
Aurelie <josserand.n.aurelie@gmail.com>
2026-03-14 16:34:39 -0400

    Updates to glb model including added details. Updates to meshes and materials.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,6 +4,7 @@ import {
   Center,
   Html,
   Environment,
+  Plane,
 } from "@react-three/drei";
 import { useState, useEffect, useRef } from "react";
 import { DoubleSide } from "three";
@@ -24,16 +25,16 @@ export default function Experience(props) {
       "/textures/Leather/Leather037_1K-JPG/Leather037_1K-JPG_NormalDX.jpg",
       "/textures/Leather/Leather037_1K-JPG/Leather037_1K-JPG_Roughness.jpg",
       "/textures/leather_red_02_coll2_1k.jpg",
-    ]
+    ],
   );
 
   const [TexColorMap, TexNormalMap, TexRoughnessMap] = useLoader(
     TextureLoader,
     [
-      "/textures/textiles/rough_linen_diff_1k.jpg",
-      "/textures/textiles/rough_linen_nor_dx_1k.jpg",
-      "/textures/textiles/rough_linen_rough_1k.jpg",
-    ]
+      "/textures/textiles/rough_linen_1k.blend/rough_linen_diff_1k.jpg",
+      "/textures/textiles/rough_linen_1k.blend/rough_linen_nor_dx_1k.jpg",
+      "/textures/textiles/rough_linen_1k.blend/rough_linen_rough_1k.jpg",
+    ],
   );
 
   const leather = (
@@ -71,76 +72,43 @@ export default function Experience(props) {
         minPolarAngle={0}
         maxPolarAngle={Math.PI}
       />
-      <ambientLight intensity={0.5} />
+      {/* Lights */}
+      {/* <ambientLight intensity={0.3} castShadow /> */}
+      <directionalLight
+        position={[2, 5, 0.5]}
+        intensity={0.01}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
 
+      {/* Geometries */}
       <Center>
+        <Plane
+          args={[20, 20]}
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, -0.25, 0]}
+          receiveShadow
+        >
+          <shadowMaterial
+            attach="material"
+            transparent
+            color="hsla(0, 0%, 51%, 1)"
+          />
+        </Plane>
         <group>
           <mesh
             castShadow
             receiveShadow
-            geometry={nodes.ThreadsTipRight003.geometry}
-            position={[0, -0.007, 0]}
-            rotation={[0, 0, -Math.PI]}
-            scale={-1}
+            geometry={nodes.ThreadsTipRight005.geometry}
           >
             {leather}
           </mesh>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Sole001.geometry}
-            material={materials.Sole}
-            position={[0, 0.844, -0.117]}
-            scale={[1.802, 0.84, 0.642]}
-          />
-
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.BackTab001.geometry}
-            position={[0, 0.844, -0.117]}
-            scale={[1.802, 0.84, 0.642]}
-          >
+          <mesh castShadow receiveShadow geometry={nodes.Sole001.geometry}>
             <meshStandardMaterial
               normalMap={brlNormalMap}
               roughnessMap={brlRoughnessMap}
-              roughness={0.9}
-              color={props.backTabColor}
-              metalness={0}
-            />
-          </mesh>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Foxing001.geometry}
-            position={[0, 0.844, -0.117]}
-            scale={[1.802, 0.84, 0.642]}
-          >
-            {leather}
-          </mesh>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Tip001.geometry}
-            position={[0, 0.844, -0.117]}
-            scale={[1.802, 0.84, 0.642]}
-          >
-            {leather}
-          </mesh>
-          <mesh castShadow receiveShadow geometry={nodes.Eyestay001.geometry}>
-            {leather}
-          </mesh>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Swoosh.geometry}
-            position={[0, 0, 0.15]}
-            rotation={[0, -0.059, 0]}
-          >
-            <meshStandardMaterial
-              normalMap={brlNormalMap}
-              roughnessMap={brlRoughnessMap}
-              roughness={0.9}
+              roughness={1}
               color={props.swooshColor}
               metalness={0}
             />
@@ -148,30 +116,136 @@ export default function Experience(props) {
           <mesh
             castShadow
             receiveShadow
-            geometry={nodes.MainBody.geometry}
-            position={[0, 0.844, -0.117]}
-            scale={[1.802, 0.84, 0.642]}
+            geometry={nodes.BottomSole001.geometry}
+            material={materials.BottomSole001}
           >
             <meshStandardMaterial
               normalMap={brlNormalMap}
               roughnessMap={brlRoughnessMap}
-              roughness={0.9}
-              color={props.mainColor}
+              roughness={1}
+              color={props.swooshColor}
               metalness={0}
             />
           </mesh>
+
           <mesh
             castShadow
             receiveShadow
             geometry={nodes.Cushion001.geometry}
-            position={[0, 0.844, -0.117]}
-            scale={[1.789, 0.834, 0.637]}
+            material={materials.BottomSole001}
           >
+            <meshStandardMaterial
+              normalMap={brlNormalMap}
+              roughnessMap={brlRoughnessMap}
+              roughness={1}
+              color={props.swooshColor}
+              metalness={0}
+            />
+          </mesh>
+
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.BackTab001.geometry}
+          >
+            <meshStandardMaterial
+              normalMap={brlNormalMap}
+              roughnessMap={brlRoughnessMap}
+              roughness={1}
+              color={props.tipEyestayTongueColor}
+              metalness={0}
+            />
+          </mesh>
+
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.BackVertical003.geometry}
+          >
+                        <meshStandardMaterial
+              normalMap={brlNormalMap}
+              roughnessMap={brlRoughnessMap}
+              roughness={1}
+              color={props.tipEyestayTongueColor}
+              metalness={0}
+            />
+          </mesh>
+
+          <mesh castShadow receiveShadow geometry={nodes.Foxing001.geometry}>
+            {leather}
+          </mesh>
+
+          <mesh castShadow receiveShadow geometry={nodes.Tip001.geometry}>
+            {leather}
+          </mesh>
+
+          <mesh castShadow receiveShadow geometry={nodes.Eyestay001.geometry}>
+            <meshStandardMaterial
+              normalMap={brlNormalMap}
+              roughnessMap={brlRoughnessMap}
+              roughness={1}
+              color={props.tipEyestayTongueColor}
+              metalness={0}
+            />
+          </mesh>
+
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.SwooshInside001.geometry}
+          >
+            <meshStandardMaterial
+              normalMap={brlNormalMap}
+              roughnessMap={brlRoughnessMap}
+              roughness={1}
+              color={props.swooshColor}
+              metalness={0}
+            />
+          </mesh>
+
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.SwooshOutside001.geometry}
+          >
+            <meshStandardMaterial
+              normalMap={brlNormalMap}
+              roughnessMap={brlRoughnessMap}
+              roughness={1}
+              color={props.swooshColor}
+              metalness={0}
+            />
+          </mesh>
+
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Tag001.geometry}
+          >
+            <meshStandardMaterial
+              normalMap={brlNormalMap}
+              roughnessMap={brlRoughnessMap}
+              roughness={1}
+              color={props.swooshColor}
+              metalness={0}
+            />
+          </mesh>
+
+          <mesh castShadow receiveShadow geometry={nodes.MainBody001.geometry}>
+            <meshStandardMaterial
+              normalMap={brlNormalMap}
+              roughnessMap={brlRoughnessMap}
+              roughness={1}
+              color={props.mainColor}
+              metalness={0}
+            />
+          </mesh>
+          <mesh castShadow receiveShadow geometry={nodes.Cushion001.geometry}>
             <meshStandardMaterial
               color={props.interiorColor}
               normalMap={TexNormalMap}
               roughnessMap={TexRoughnessMap}
-              roughness={0.99}
+              roughness={1}
               metalness={0}
             />
           </mesh>
@@ -179,60 +253,50 @@ export default function Experience(props) {
             castShadow
             receiveShadow
             geometry={nodes.TongueCushion001.geometry}
-            position={[0, 0.844, -0.117]}
-            scale={[1.802, 0.84, 0.642]}
           >
             <meshStandardMaterial
               normalMap={brlNormalMap}
               roughnessMap={brlRoughnessMap}
-              roughness={0.9}
+              roughness={1}
               color={props.tipEyestayTongueColor}
               metalness={0}
             />
           </mesh>
-          <mesh
+                    <mesh
             castShadow
             receiveShadow
-            geometry={nodes.Plane001.geometry}
-            position={[0.408, 0.414, 0.004]}
-            rotation={[0, 0, 0.459]}
+            geometry={nodes.TongueTab001.geometry}
           >
-                        <meshStandardMaterial
+            <meshStandardMaterial
               normalMap={brlNormalMap}
               roughnessMap={brlRoughnessMap}
-              roughness={0.9}
+              roughness={1}
+              color={props.tipEyestayTongueColor}
+              metalness={0}
+            />
+          </mesh>
+          <mesh castShadow receiveShadow geometry={nodes.Lace001.geometry}>
+            <meshStandardMaterial
               color={props.laceColor}
+              normalMap={TexNormalMap}
+              roughnessMap={TexRoughnessMap}
+              roughness={1}
               metalness={0}
             />
           </mesh>
           <mesh
             castShadow
             receiveShadow
-            geometry={nodes.SoleAir002.geometry}
-            position={[0, 0.844, -0.117]}
-            scale={[1.794, 0.836, 0.639]}
+            geometry={nodes.SoleAirLine001.geometry}
           >
             {leather}
           </mesh>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.SoleAir003.geometry}
-            position={[0, 0.844, -0.117]}
-            scale={[1.794, 0.836, 0.639]}
-          >
+          <mesh castShadow receiveShadow geometry={nodes.Air001.geometry}>
             {leather}
           </mesh>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Air001.geometry}
-            position={[1.366, 0.277, 0.502]}
-            rotation={[Math.PI / 2, 0, 0]}
-            scale={0.12}
-          >
-            {leather}
-          </mesh>
+
+      {/* Threads */}
+
         </group>
         <Environment preset="city" />
       </Center>
